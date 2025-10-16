d218c8774f742a3db487d193b636d1641fd597fa
Armel Sandjong MBA <137562527+Armelsandjong@users.noreply.github.com>
2025-10-16 22:02:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
