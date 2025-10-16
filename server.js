130f1ffdc3379dc25e2e32f9098ceb3bd6ffe31a
Armel Sandjong MBA <137562527+Armelsandjong@users.noreply.github.com>
2025-10-16 20:55:19 +0200

    Fix static file serving with path.join

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public')); // This serves files from the public folder
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
